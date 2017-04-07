8bdc5f90ffc4ba532ec26cdfc5eec1d0b5a79183
Sam Wang <sam.zz.wang@gmail.com>
2017-04-07 13:33:17 -0700

    Implement IntSet, Hashing

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,22 +1,30 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
+    @max = max
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num < @max && num >= 0
   end
 
   def validate!(num)
+    raise 'Out of bounds' unless is_valid?(num)
   end
 end
 
@@ -27,18 +35,21 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +66,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      resize! if num_buckets == count
+
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +93,13 @@ class ResizingIntSet
   end
 
   def resize!
+    store = Array.new(num_buckets * 2) { Array.new }
+    (0..num_buckets - 1).each do |bucket_i|
+      until self[bucket_i].empty?
+        num = self[bucket_i].last
+        store[num % (num_buckets * 2)] << self[bucket_i].pop
+      end
+    end
+    @store = store
   end
 end
